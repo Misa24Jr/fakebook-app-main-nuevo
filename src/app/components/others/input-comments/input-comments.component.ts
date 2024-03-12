56f53044c1a7d4fd0f4a22243711030e9a12ae72
Misa24jr <misa24jr@gmail.com>
2024-03-12 13:27:47 -0400

    Mas cambios en el modal

--- src/app/components/others/input-comments/input-comments.component.ts
+++ src/app/components/others/input-comments/input-comments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage, ref, uploadBytes } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-input-comments',
@@ -8,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputCommentsComponent  implements OnInit {
 
-  constructor() { }
+  uploadImage(event: any){
+    const file = event.target.files[0];
+    console.log(file);
+
+    const imgRef = ref(this.storage, `images/${file.name}`);
+
+    uploadBytes(imgRef, file)
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+  }
+
+
+
+  constructor(private storage: Storage) { }
 
   ngOnInit() {}
 
