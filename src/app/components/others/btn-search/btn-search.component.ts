71f9880924596bf418544749882ff62ceec9eb1b
josemavarez11 <josemmr311@gmail.com>
2024-03-13 19:52:16 -0400

    get comments method done

--- src/app/components/others/btn-search/btn-search.component.ts
+++ src/app/components/others/btn-search/btn-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-btn-search',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   standalone: true
 })
 export class BtnSearchComponent  implements OnInit {
+  @Input() user: any = {};
 
   constructor() { }
 
